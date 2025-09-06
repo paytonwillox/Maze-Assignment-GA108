8e22073e28916823b94363abc0312cf8014f78f4
xopdubxo <paytonwillox@gmail.com>
2025-09-06 00:25:34 -0700

    meow meow everything

---
+++ Assets/scripts/move kitty #2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movekitty : MonoBehaviour
+{
+    public float movespeed = 5f;
+    private Rigidbody rb;
+    private Vector3 move;
+    //im tryna make my kitty run
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        move = new Vector3(h, 0, v);
+        transform.Translate(move* movespeed * Time.deltaTime);
+    }
+}
